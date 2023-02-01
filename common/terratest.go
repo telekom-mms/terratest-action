45446ff29b0b3370a81c883884c3de5fdb6ae310
michaelamattes <michaela.mattes@t-systems.com>
2023-02-01 16:07:39 +0100

    add functions and code for integration test

--- common/terratest.go
+++ common/terratest.go
@@ -2,7 +2,10 @@ package common
 
 import (
 	"os"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
 	"gopkg.in/yaml.v3"
 )
 
@@ -23,4 +26,35 @@ func GetTerratestSettings(file string) TerratestSettings {
 	yaml.NewDecoder(f).Decode(&settings)
 
 	return *settings
+}
+
+func AssertTrue(t *testing.T, exists bool) {
+	assert.True(t, exists)
+}
+
+func AssertEqual(t *testing.T, options any, values string) {
+	for key, value := range options.(map[string]interface{}) {
+		jsonValue := gjson.Get(values, key).Value()
+		assert.Equal(t, value, jsonValue)
+	}
+}
+
+func GetValues(json string, jsonQuery string) string {
+	jsonValues := gjson.Get(json, jsonQuery)
+
+	var getValues string
+
+	for _, array1 := range jsonValues.Array() {
+		for _, array2 := range array1.Array() {
+			getValues = array2.String()
+		}
+	}
+
+	return getValues
+}
+
+func GetValue(json string, jsonQuery string) string {
+	getValue := gjson.Get(json, jsonQuery).String()
+
+	return getValue
 }
--- terratest/azure_test.go
+++ terratest/azure_test.go
@@ -1,10 +1,12 @@
 package terratest
 
 import (
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"os"
+	"strings"
 	"terratest-action/common"
 	"testing"
-
-	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestAzure(t *testing.T) {
@@ -16,6 +18,10 @@ func TestAzure(t *testing.T) {
 	testSettings := common.GetTestSettings()
 	path := testSettings["path"]
 
+	// get terratest settings
+	terratestSettings := path + "/terratest.yaml"
+	settings := common.GetTerratestSettings(terratestSettings)
+
 	// prepare Terratest Seetings
 	// website::tag::1:: Configure Terraform setting up a path to Terraform code.
 	terraformOptions := &terraform.Options{
@@ -23,8 +29,51 @@ func TestAzure(t *testing.T) {
 	}
 
 	// unit tests
-	common.LogColor("yellow", "Unit Tests")
+	common.LogColor("yellow", "Unit Test")
 
 	// website::tag::2:: Run `terraform init` and `terraform plan`. Fail the test if there are any errors.
 	terraform.InitAndPlan(t, terraformOptions)
+
+	// integration tests
+	common.LogColor("yellow", "Integration Test")
+
+	// website::tag::5:: At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// website::tag::2:: Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	for _, function := range settings.Functions {
+		functionCase := strings.Contains(function, "Exists")
+		options := settings.Options[function]
+
+		if functionCase == false && options == nil {
+			common.LogMiss("options for function " + function)
+		} else {
+			common.LogColor("yellow", "> "+function)
+
+			// website::tag::3:: Run `terraform show` to get the values after build
+			tfShow := terraform.Show(t, terraformOptions)
+
+			jsonQuery := "values.root_module.child_modules.#.resources.#.values"
+			tfValues := common.GetValues(tfShow, jsonQuery)
+
+			resourceGroupName := common.GetValue(tfValues, "resource_group_name")
+			resourceName := common.GetValue(tfValues, "name")
+			subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+
+			// website::tag::4:: Assert
+			switch function {
+			case "ContainerRegistryExists":
+				exists := azure.ContainerRegistryExists(t, resourceName, resourceGroupName, subscriptionID)
+				common.AssertTrue(t, exists)
+
+			case "GetContainerRegistry":
+				common.AssertEqual(t, options, tfValues)
+
+			default:
+				common.LogMiss(function)
+			}
+		}
+	}
 }
